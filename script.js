665dc3d601cccc9784fe0674cef5a565b17b76cd
marat0140 <maratcamargo06@gmail.com>
2024-10-17 15:14:08 -0500

    message

---
+++ script.js
@@ -0,0 +1,26 @@
+async function buscarPersona() {
+    const query = document.getElementById('search').value;
+    const response = await fetch(`http://localhost:3000/search?id=${query}&nombre=${query}`);
+    const personas = await response.json();
+
+    const lista = document.getElementById('resultados');
+    lista.innerHTML = '';
+
+    personas.forEach(persona => {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td>${persona.id}</td>
+        <td>${persona.nombre}</td>
+        <td>${persona.apellido}</td>
+        <td>${persona.nacionalidad}</td>
+        <td>${persona.fecha_nacimiento}</td>
+        <td>${persona.estatura} cm</td>
+        <td>${persona.genero}</td>
+        <td>${persona.edad}</td>
+        <td>${persona.grupo_sanguineo}</td>
+        <td>${persona.user_discord}</td>
+        <td>${persona.user_roblox}</td>
+      `;
+      lista.appendChild(tr);
+    });
+  }
